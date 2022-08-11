bb2e69b8d6c0fcc79145bb73e9bdb7f37e296694
Leonir Júnior Ribeiro Calestro <105824089+calestro@users.noreply.github.com>
2022-08-11 00:24:13 -0300

    criando o esqueleto do projeto

--- lib/activy/main_page.dart
+++ lib/activy/main_page.dart
@@ -13,44 +13,49 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final wd = MediaQuery.of(context).size.width;
+    final hg = MediaQuery.of(context).size.height;
     StyleMain style = StyleMain();
 
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: EdgeInsets.all(60),
-        child: Stack(
-            children: [
-
-          //Titulo
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Myname(myName: "Leonir", mySurname: "Júnior"),
-              Text("Developer", style: style.infoGeral),
-            ],
-          ),
-
-          //contact
-          Positioned(
-            top: 35,
-            left: wd * 0.5,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: Container(
+        width: wd,
+        height: hg,
+        child: Padding(
+          padding: EdgeInsets.all(60),
+          child: Stack(
               children: [
-                Text("(22) 992131045  ", style: style.infoGeral,),
-                Text("calestrojunior@gmail.com  ",style: style.infoGeral),
-                Text("Rua São Paulo N° 256  ",style: style.infoGeral),
+
+            //Titulo
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Myname(myName: "Leonir", mySurname: "Júnior"),
+                Text("Developer", style: style.infoGeral),
               ],
             ),
-          ),
 
-          Row(
-            children: [],
-          ),
-        ]),
+            //contact
+            Positioned(
+              top: 80,
+              left: wd * 0.5,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("(22) 992131045  ", style: style.infoGeral,),
+                  Text("calestrojunior@gmail.com  ",style: style.infoGeral),
+                  Text("Rua São Paulo N° 256  ",style: style.infoGeral),
+                ],
+              ),
+            ),
+
+            Row(
+              children: [],
+            ),
+          ]),
+        ),
       ),
     );
   }
