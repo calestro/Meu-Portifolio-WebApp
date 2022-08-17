0fecf1521f1dba0b160dfa527c109342490a8369
Leonir Júnior Ribeiro Calestro <105824089+calestro@users.noreply.github.com>
2022-08-17 00:47:30 -0300

    adicionando arquivo de widget

---
+++ lib/activy/big_device/windows_maker/list_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:port_leonir/activy/big_device/windows_maker/resizable_window.dart';
+
+class ListWidget{
+  static final ListWidget _listWidget = ListWidget._internal();
+  factory ListWidget (){
+    return _listWidget;
+  }
+  ListWidget._internal();
+
+  List<ResizableWindow> windowsOpenned = [];
+  bool isMinimmize = false;
+  Map<Key?,double> savePostion = {};
+  // ignore: non_constant_identifier_names
+  Map<String, String> IconAplication = {
+    "Minhas Skills":"img/skill_icon.png"
+
+
+  };
+}
